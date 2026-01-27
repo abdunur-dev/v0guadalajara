322a90d1b96509c42f0ac0db05f8c532eed08d30
Juan Martinez <juanda.martinezn@gmail.com>
2026-01-26 18:45:04 -0600

    feat: Agenda section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/hero-section";
 import Features from "@/components/features-3";
+import Agenda from "@/components/agenda";
 
 export default function Home() {
     return (
         <>
             <HeroSection/>
             <Features/>
+            <Agenda/>
         </>
     )
 }
